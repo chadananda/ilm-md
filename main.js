bfcad7987991077f214f58df05e7cd17e1a3be8c
Chad <chadananda@gmail.com>
2018-03-17 10:59:13 -0700

    test update

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ var jsyaml = require('yaml-front-matter')
 var css = require('./main.css')
 
 var markdownItComments = require('markdown-it-inline-comments')
-var markdownItAttrs = require('markdown-it-attrs') 
-// var markdownItReplacements = require('markdown-it-replacements')  
-//var markdownItUnderline = require('markdown-it-underline') 
-var markdownItFootnote = require('markdown-it-footnote') 
+var markdownItAttrs = require('markdown-it-attrs')
+// var markdownItReplacements = require('markdown-it-replacements')
+//var markdownItUnderline = require('markdown-it-underline')
+var markdownItFootnote = require('markdown-it-footnote')
 var markdownItTableOfContents = require('markdown-it-toc-ilm') // my custom version
-var markdownItAnchor = require('markdown-it-anchor') 
+var markdownItAnchor = require('markdown-it-anchor')
 var markdownItParnum = require('markdown-it-parnum')
 
 var md = require('markdown-it')({
@@ -20,24 +20,24 @@ var md = require('markdown-it')({
     quotes: '“”‘’',
   })
   .use(markdownItComments)
-  .use(markdownItAnchor)  
+  .use(markdownItAnchor)
   //.use(markdownItReplacements)
   //.use(markdownItUnderline)
-  //.use(markdownItFootnote)
+  .use(markdownItFootnote)
   .use(markdownItParnum)
   .use(markdownItTableOfContents, {
     "includeLevel": [2,3,4,5],
     "containerClass": 'toc',
     "format": function(title) {
       // remove attributes from display
-      return title.replace(/\{.*?\}[\s]*$/,'')    
+      return title.replace(/\{.*?\}[\s]*$/,'')
     }
   })
   .use(markdownItAttrs)
-  
 
 
-module.exports = function(markdownText) { 
+
+module.exports = function(markdownText) {
   var ilm = {
     raw_md: markdownText,
     attrs: {},
@@ -47,11 +47,12 @@ module.exports = function(markdownText) {
     html_head: '',
     html: ''
   }
-  // parse here please  
-  ilm.attrs = jsyaml.parse(markdownText.trim())  
+  // parse here please
+  ilm.attrs = jsyaml.parse(markdownText.trim())
   ilm.raw_content = ilm.attrs.__content.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
   delete(ilm.attrs.__content)
   var html = md.render(ilm.raw_content)
+  /*
   // glyph underscores
     .replace(/_([kcgsdzt]h)[_]?/ig, '<u>$1</u>')
     .replace(/<em>([kcgsdzt]h)<\/em>/ig, '<u>$1</u>')
@@ -60,18 +61,18 @@ module.exports = function(markdownText) {
     .replace(/\^[u]/g, 'ú').replace(/\^[U]/g, 'Ú')
     .replace(/\^[a]/g, 'á').replace(/\^[A]/g, 'Ú')
   // glyph dot-unders
-    .replace(/\.[h]/g, 'ḥ').replace(/(\s)\.[H]/g, '$1Ḥ') 
-    .replace(/\.[s]/g, 'ṣ').replace(/(\s)\.[S]/g, '$1Ṣ') 
-    .replace(/\.[d]/g, 'ḍ').replace(/(\s)\.[D]/g, '$1Ḍ') 
-    .replace(/\.[t]/g, 'ṭ').replace(/(\s)\.[T]/g, '$1Ṭ')  
+    .replace(/\.[h]/g, 'ḥ').replace(/(\s)\.[H]/g, '$1Ḥ')
+    .replace(/\.[s]/g, 'ṣ').replace(/(\s)\.[S]/g, '$1Ṣ')
+    .replace(/\.[d]/g, 'ḍ').replace(/(\s)\.[D]/g, '$1Ḍ')
+    .replace(/\.[t]/g, 'ṭ').replace(/(\s)\.[T]/g, '$1Ṭ')
   // glyph style ayn and hamza
-    .replace(/\\6/g, '‘').replace(/\\9/g, '’') 
+    .replace(/\\6/g, '‘').replace(/\\9/g, '’')
   // fix un-fixed straight quotes (be careful, we are manipulating raw HTML)
-    .replace(/[^\\]\'([\S])/g, '‘$1').replace(/[^\\]\'/g, '’') 
-  
-  
-  ilm.html_body = html 
-  ilm.html = html 
+    .replace(/[^\\]\'([\S])/g, '‘$1').replace(/[^\\]\'/g, '’')
+  */
+
+  ilm.html_body = html
+  ilm.html = html
   return ilm
 }
- 
+
